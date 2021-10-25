6290ab157938ed1d2fa6b3ddf6ececbee2f7a63c
flayner2 <flayner5@gmail.com>
2021-10-25 18:43:41 -0300

    Added a set of utility functions, removed unecessary variables

--- taiga/common/taxon.py
+++ taiga/common/taxon.py
@@ -6,14 +6,16 @@ class Taxon:
     A Taxon is a representation of the taxonomic information of an organism.
     """
 
+    # Class variable declarations
+
     name: str
     genome_id: int
     taxon_id: int
     classification: Dict  # This may become a `defaultdict`
-    missing_name: bool
-    missing_taxon_id: bool
-    missing_corrected: bool
-    missing_classification: bool
+
+    ###########################################################
+
+    # Dunder methods
 
     def __init__(
         self,
@@ -21,44 +23,50 @@ class Taxon:
         genome_id: int = 0,
         taxon_id: int = 0,
         classification: Dict = {},
-        missing_name: bool = False,
-        missing_taxon_id: bool = False,
-        missing_corrected: bool = False,
-        missing_classification: bool = False,
-    ) -> None:
+    ):
         self.name = name
         self.genome_id = genome_id
         self.taxon_id = taxon_id
         self.classification = classification
-        self.missing_name = missing_name
-        self.missing_taxon_id = missing_taxon_id
-        self.missing_corrected = missing_corrected
-        self.missing_classification = missing_classification
 
-    def list_taxa(self) -> List:
-        """Returns a list of all the names of the taxon ranks for an organism
+    def __lt__(self, other) -> bool:
+        pass
+        # if self.taxon_id > 0 and other.taxon_id > 0:
+        # return self.taxon_id < other.taxon_id
+        # elif self.name and other.name:
+        # return self.name < other.name
+        # elif self.genome_id > 0 and
 
-        Parameters:
-        self (object): A reference to the object itself
+    ###########################################################
+
+    # Getters and setters
+
+    def get_taxon_names_list(self) -> List:
+        """
+        @getter
+
+        Returns a list of all the names of the taxon ranks for an organism.
 
         Returns:
-        (list): A list of all the values from the 'self.classification' dict
-
+            (list): A list of all the values from the `self.classification` dict.
         """
 
-        if self.classification is not None:
+        if self.classification:
             return list(self.classification.values())
-        else:
-            return []
 
-    def list_ranks(self) -> Set:
-        """Returns a set with all the ranks for an organism
+        return []
+
+    def get_taxon_ranks_list(self) -> Set:
+        """
+        @getter
+
+        Returns a set with all the ranks for an organism.
 
         Parameters:
-        self (object): A reference to the object itself
+            self (object): A reference to the object itself.
 
         Returns:
-        (set): A set of all the unique keys from the 'self.classification' dict
+            (set): A set of all the unique keys from the `self.classification` dict.
 
         """
 
@@ -66,3 +74,47 @@ class Taxon:
             return set(self.classification)
         else:
             return set()
+
+    ###########################################################
+
+    # Utility methods
+
+    def has_name(self) -> bool:
+        """
+        Checks if the Taxon has an organism name.
+
+        Returns:
+            (bool): True if the Taxon has a name, else False.
+        """
+
+        return bool(self.name)
+
+    def has_taxon_id(self) -> bool:
+        """
+        Checks if the Taxon has a TaxonID (meaning it is > 0).
+
+        Returns:
+            (bool): True if the Taxon has a TaxonID, else False.
+        """
+
+        return bool(self.taxon_id)
+
+    def has_genome_id(self) -> bool:
+        """
+        Checks if the Taxon has an genome id (meaning it is > 0).
+
+        Returns:
+            (bool): True if the Taxon has a genome id, else False.
+        """
+
+        return bool(self.genome_id)
+
+    def has_classification(self) -> bool:
+        """
+        Checks if the Taxon has a classification dictionary.
+
+        Returns:
+            (bool): True if the Taxon has a classification, else False.
+        """
+
+        return bool(self.classification)
