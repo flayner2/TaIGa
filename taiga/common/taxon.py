b868e9f0987353eac8e9dd79ce828fd90bc6ad2e
flayner2 <flayner5@gmail.com>
2022-03-12 20:14:14 -0300

    Fixed a typo

--- taiga/common/taxon.py
+++ taiga/common/taxon.py
@@ -99,7 +99,7 @@ class Taxon:
 
     ###########################################################
 
-    # Getters and setters - only for compound data likes dicts and lists
+    # Getters and setters - only for compound data like dicts and lists
 
     def get_taxon_names_list(self) -> List:
         """
